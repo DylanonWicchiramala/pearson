498f6ffdf47d291623c27d8dc4f483b7e3c1038f
DylanonWicchiramala <43574914+DylanonWicchiramala@users.noreply.github.com>
2020-11-12 19:14:14 +0700

    login_bot.py complete

--- login_bot.py
+++ login_bot.py
@@ -2,27 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-
-
-usrname = '63050131'
-psd = open('pwd.txt','r').read()
+username = '63050131'  # type pearson user name here
+try:
+    psd = open('pwd.txt', 'r').read()  # create pwd.txt file and type your password
+except:
+    print('pwd.txt not found , please create pwd.txt file and type your password')
 
 driver = webdriver.Chrome('C:/Users/Dylan/chromedriver_win32/chromedriver.exe')
 driver.get("https://myenglishlab.pearson-intl.com/assignments/#!/allCourses/allStatuses")
 
-sleep(2.9)    #loading time
+sleep(2.9)  # delays for loading time.
 loop_times = 1
 while True:
-    if loop_times==4 : break
+    loop_times += 1
     try:
         print("Now logging... ")
 
-        driver.find_element_by_name('name').send_keys(usrname)
-        sleep(0.03)
+        driver.find_element_by_name('name').send_keys(username)
         driver.find_element_by_name('password').send_keys(psd)
-        #driver.find_element_by_name('password').send_keys(Keys.RETURN)
+        driver.find_element_by_name('password').send_keys(Keys.RETURN)
         driver.maximize_window()
         break
     except:
-        print("some thing wrong trying again...")
+        print("something wrong trying again...")
         sleep(0.2)
+        if loop_times == 5: print('something went wrong.'); break
