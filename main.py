d6f31d7a8ae05b6903b3f28062140da30197c23c
DylanonWicchiramala <43574914+DylanonWicchiramala@users.noreply.github.com>
2020-11-12 20:58:04 +0700

    change filename autologin to operation , add openAssignment define , at login add fulscreen attribute

--- main.py
+++ main.py
@@ -3,18 +3,10 @@ from selenium import webdriver
 import bs4
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-from login_bot import login
+import operation
 from get_driver import driver
 import sys
+from time import sleep
 
-login()
-
-sys.exit()
-
-time.sleep(8)  # delays program for load website and login.
-while True:
-    time.sleep(1)
-    try:
-        break
-    except:
-        pass
+operation.login()
+operation.openAssignment()
--- operation.py
+++ operation.py
@@ -8,27 +8,42 @@ import sys
 # driver = webdriver.Chrome('C:/Users/Dylan/chromedriver_win32/chromedriver.exe')
 # driver.get("https://myenglishlab.pearson-intl.com/assignments/#!/allCourses/allStatuses")
 
-def login(username='63050131',retireTimes=10):
+def login(username='63050131', retireTimes=10, fullScreen= True):
     # get usermname and password
     username = username  # type pearson user name here
     try:
         pwd = open('pwd.txt', 'r').read()  # create pwd.txt file and type your password
     except:
-        print('pwd.txt not found , please create pwd.txt file and type your password')
+        print('pwd.txt not found, please create pwd.txt file and type your password in it.')
 
     # login
     sleep(2.9)  # delays for loading time.
     loop_times = 1
-    print("Now logging...")
+    print("login: Now logging...")
     for i in range(retireTimes):
         loop_times += 1
         try:
             driver.find_element_by_name('name').send_keys(username)
             driver.find_element_by_name('password').send_keys(pwd)
             driver.find_element_by_name('password').send_keys(Keys.RETURN)
-            driver.maximize_window()
+            if fullScreen: driver.maximize_window()
             break
         except:
-            #print("trying again.")
+            print("login: logging again.")
             sleep(0.7)
-    print('something went wrong.')
+    if i == retireTimes - 1: ('login: something went wrong.')
+    sleep(6)  # assignment loadding time
+
+
+def openAssignment(retireTimes=50, at=1):
+    at = at - 1
+    sleep(3)  # delay for load assignment.
+    print('openAssignment: clicking Ex.')
+    for i in range(retireTimes):
+        try:
+            driver.find_element_by_id('timeline-item-link').get_attribute('herf') #get herf
+            break
+        except:
+            print('openAssignment: clicking again.')
+            sleep(0.7)
+    if i == retireTimes - 1: print('openAssignment: something went wrong.')
