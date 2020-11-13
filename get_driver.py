e2b753e543476c03b10a02491fb8237f7f5dd22b
DylanonWicchiramala <43574914+DylanonWicchiramala@users.noreply.github.com>
2020-11-13 09:44:31 +0700

    chrome driver at project file

--- get_driver.py
+++ get_driver.py
@@ -1,3 +1,3 @@
 from selenium import webdriver
-driver = webdriver.Chrome('C:/Users/Dylan/chromedriver_win32/chromedriver.exe')
+driver = webdriver.Chrome()
 driver.get('https://myenglishlab.pearson-intl.com/assignments/#!/allCourses/allStatuses')
